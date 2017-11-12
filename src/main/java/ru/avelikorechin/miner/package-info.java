12929dc6fb9d6c27a68a339f1a49ab752603389f
Mofin52 <a.velikorechin@gmail.com>
2017-11-12 22:49:30 +0300

    ititial commit

---
+++ src/main/java/ru/avelikorechin/miner/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Windows-like miner game classes.
+ * @author Alexander Velikorechin
+ * @since 11.11.2017
+ */
+package ru.avelikorechin.miner;
---
+++ src/test/java/ru/avelikorechin/miner/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Windows-like miner test classes.
+ * @author Alexander Velikorechin
+ * @since 11.11.2017
+ */
+package ru.avelikorechin.miner;
