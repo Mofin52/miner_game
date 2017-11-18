32127ca9d316caabe82939ce9e85a02d0d774278
Mofin52 <a.velikorechin@gmail.com>
2017-11-19 02:42:51 +0300

    finished visual appearance of app

--- src/main/java/ru/avelikorechin/miner/StartUI.java
+++ src/main/java/ru/avelikorechin/miner/StartUI.java
@@ -7,7 +7,7 @@ import ru.avelikorechin.miner.minerUI.BasicUI;
  */
 public class StartUI {
     public static void main(String[] args) {
-        Board board = new Board(10, 10, 10);
+        Board board = new Board(20, 20, 10);
         board.fillBoard();
         for (int i = 0; i < board.getCells().length; i++) {
             for (int j = 0; j < board.getCells()[i].length; j++){
@@ -16,6 +16,6 @@ public class StartUI {
             System.out.println();
         }
         BasicUI ui = new BasicUI(16, 16, board.getCells());
-        ui.initUi(20, 10);
+        ui.initUi();
     }
 }
--- src/main/java/ru/avelikorechin/miner/minerUI/BasicUI.java
+++ src/main/java/ru/avelikorechin/miner/minerUI/BasicUI.java
@@ -2,9 +2,13 @@ package ru.avelikorechin.miner.minerUI;
 
 import ru.avelikorechin.miner.cells.Cell;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.plaf.IconUIResource;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Interface for miner game.
@@ -39,21 +43,37 @@ public class BasicUI {
 
     /**
      * Method to init user interface.
-     * @param columns number of columns required
-     * @param rows number of row required
      */
-    public void initUi(int columns, int rows) {
+    public void initUi() {
         JFrame frame = new JFrame("Minesweeper Game");
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(columns*cellWidth, rows*cellHeight);
+        drawFrame(frame);
+        frame.getContentPane().setPreferredSize(new Dimension(cellHeight*cells[0].length, cellWidth*cells.length));
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.pack();
+
     }
 
     private void drawFrame(JFrame frame){
-        //TODO the method must create needed ammount of elements, set their content, add them to frame
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                Image image = null;
+                try {
+                    File url = new File("src\\main\\java\\ru\\avelikorechin\\miner\\minerUI\\resources\\hidden.png");
+                    image = ImageIO.read(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+                JLabel label = new JLabel(new ImageIcon(image));
+                label.setBounds(cellWidth*j,cellHeight*i,cellWidth,cellHeight);
+                label.setBackground(Color.BLACK);
+                label.setVisible(true);
+                frame.getContentPane().add(label);
+            }
+        }
     }
 }
